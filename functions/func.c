68aa6f4638fe3b93fd769d8af2bedd5f04173c7c
deepaksy <deepaksy28@gmail.com>
2023-09-17 16:34:16 +0530

    update

--- functions/func.c
+++ functions/func.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
+char *name(){
+    char *ch1;
+    ch1 = (char*) malloc(sizeof(char)*7);
+    const char* value = "Deepak";
+    strncpy(ch1,value,strlen(value));
+    ch1[strlen(value)] = '\0';
+    // printf("%s\n",name);
+    return ch1;
+}
 int main(){
+    printf("%s",name());
     return 0;
 }
