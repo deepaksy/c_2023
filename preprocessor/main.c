512519e2b8d9a75ea158127089f7c0e3f266e9a3
deepaksy <deepaksy28@gmail.com>
2023-09-18 18:27:05 +0530

    initial commit

--- preprocessor/main.c
+++ preprocessor/main.c
@@ -6,5 +6,6 @@ int main(){
     int area;
     area = __AREA(3,4);
     printf("Area: %d",area);
+    printf("%s",PLD("ksdfjlds"));
     return 0;
 }
--- preprocessor/main.h
+++ preprocessor/main.h
@@ -2,4 +2,5 @@
 
 #define __AREA(l, b) (l * b)
 // #undef __AREA
+#define PLD(code...)  code
 #endif
