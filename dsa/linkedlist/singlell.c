7d8ecf1860d78f84bc78e5636dbe8dd2638a1dc6
deepaksy <deepaksy28@gmail.com>
2023-09-21 11:23:27 +0530

    Add linked list functions

--- dsa/linkedlist/singlell.c
+++ dsa/linkedlist/singlell.c
@@ -20,6 +20,21 @@ struct node * newNode(int data){
     return newNode;
 }
 
+int search(struct node* ll,int el){
+    struct node* temp;
+    int count=0;
+    temp=ll;
+    while(temp!=NULL){
+        count++;
+        if(temp->data==el){
+            return count;
+        }
+        temp=temp->next;
+    }
+    return -1;
+
+}
+
 void traverse(struct node *ptr){
     printf("Traversal");
     if(ptr->next==NULL){
@@ -29,6 +44,54 @@ void traverse(struct node *ptr){
         printf("\nData: %d\n",ptr->data);
         ptr=ptr->next;
     }while(ptr!=NULL);
+}
+
+int length(struct node* ptr){
+    int count=0;
+    struct node* current = ptr;
+    while(current!=NULL){
+        count++;
+        current=current->next;
+    }
+    return count;
+}
+
+int getcount(struct node* ptr){
+    if(ptr==NULL){
+        return 0;
+    }
+    else{
+        return 1+getcount(ptr->next);
+    }
+}
+
+int reverse(struct node** list){
+    struct node * current = head;
+    struct node * prev=NULL,*next=NULL;
+
+    while(current!=NULL){
+        next=current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+}
+
+int deletenode(struct node *head,int el){
+    struct node *tmp,*prev=NULL;
+    tmp = head;
+    do{
+        if(tmp->data==el){
+            prev->next = tmp->next;
+            free(tmp);
+            return 1;
+        }
+        else{
+            prev=tmp;
+            tmp=tmp->next;
+        }
+    }while(tmp!=NULL);
+    return -1;
 }
 
 int main(){
@@ -44,9 +107,29 @@ int main(){
             node->next=NULL;
         }
     }
-    // printf("\nHead: %d",head->data);
-    // printf("\n Next node addr: %p",head->next);
     traverse(head);
+    int sel;
+    printf("\nEnter the element to search:");
+    scanf("%d",&sel);
+    int res = search(head,sel);
+    (sel!=-1)?printf("Element found at index: %d",res):printf("Element not found!");
+    printf("\nLength of Linked list: %d",length(head));
+    printf("\nLength of Linked list(Recursively): %d",getcount(head));
+    printf("\nReverse a linked list:\n");
+    reverse(&node);
+    head = node;
+    traverse(head);
+    int delel;
+    printf("\nEnter the element to delete: ");
+    scanf("%d",&delel);
+    int delstat = deletenode(head,delel);
+    if(delstat>0){
+        printf("\nElement %d Deleted from linked list",delel);
+        traverse(head);
+    }
+    else{
+        printf("\nElement %d not found in linked list.",delel);
+    }
     // struct node *temp = head->next;
     // printf("\n data: %d",temp->data);
     return 0;
